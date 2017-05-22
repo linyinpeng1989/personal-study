edd14512548cd5b74fa7a58a10486a7ef8536abb
linyp <linyp@guahao.com>
2017-05-22 15:35:47 +0800

    优化

--- java_8/src/main/java/com/linyp/study/utils/ProtoStuffUtils.java
+++ java_8/src/main/java/com/linyp/study/utils/ProtoStuffUtils.java
@@ -2,13 +2,35 @@ package com.linyp.study.utils;
 
 import com.dyuproject.protostuff.LinkedBuffer;
 import com.dyuproject.protostuff.ProtobufIOUtil;
+import com.dyuproject.protostuff.Schema;
 import com.dyuproject.protostuff.runtime.RuntimeSchema;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by linyp on 2017/5/21.
  * 序列化工具
  */
 public class ProtoStuffUtils {
+    private static Map<Class<?>, Schema<?>> cachedSchema = new ConcurrentHashMap<>();
+
+    /**
+     * 获取Schema对象
+     * @param cls
+     * @param <T>
+     * @return
+     */
+    private static <T> Schema<T> getSchema(Class<T> cls) {
+        Schema<T> schema = (Schema<T>) cachedSchema.get(cls);
+        if (schema == null) {
+            schema = RuntimeSchema.createFrom(cls);
+            if (schema != null) {
+                cachedSchema.put(cls, schema);
+            }
+        }
+        return schema;
+    }
 
     /**
      * 序列化
@@ -16,9 +38,9 @@ public class ProtoStuffUtils {
      * @return
      */
     public static <T> byte[] toBytes(T obj) {
-        Class clz = obj.getClass();
-        RuntimeSchema<T> schema = RuntimeSchema.createFrom(clz);
-        return ProtobufIOUtil.toByteArray(obj,schema, LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE));
+        Class<T> clz = (Class<T>) obj.getClass();
+        Schema<T> schema = getSchema(clz);
+        return ProtobufIOUtil.toByteArray(obj, schema, LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE));
     }
 
     /**
@@ -28,7 +50,7 @@ public class ProtoStuffUtils {
      * @return
      */
     public static <T> T toObject(byte[] bytes, Class<T> clz) {
-        RuntimeSchema<T> schema = RuntimeSchema.createFrom(clz);
+        Schema<T> schema = getSchema(clz);
         T obj = schema.newMessage();
         ProtobufIOUtil.mergeFrom(bytes, obj, schema);
         return obj;
