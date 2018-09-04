a8146284d330fe47dc417262c9f11f5d1f8aa0e2
Yinpeng.Lin@geely.com <ZXCzxc654321>
2018-09-04 15:10:37 +0800

    forkjoin

--- concurrency/src/main/java/com/linyp/study/forkjoin/Main.java
+++ concurrency/src/main/java/com/linyp/study/forkjoin/Main.java
@@ -11,13 +11,13 @@ import java.util.concurrent.TimeUnit;
  */
 public class Main {
     public static void main(String[] args) {
-        ProductListGenerator generator=new ProductListGenerator();
+        ProductListGenerator generator = new ProductListGenerator();
         // 创建一个包括10000个产品的列表
-        List<Product> products=generator.generate(10000);
+        List<Product> products = generator.generate(10000);
         // .创建一个新的Task对象，用来更新产品队列中的产品。first参数使用值0，last参数使用值10000
         Task task = new Task(products, 0, products.size(), 0.20);
         // 使用无参构造器创建ForkJoinPool对象
-        ForkJoinPool pool=new ForkJoinPool();
+        ForkJoinPool pool = new ForkJoinPool();
         // 在池中使用execute()方法执行这个任务，execute()方法异步执行
         pool.execute(task);
 
@@ -35,12 +35,12 @@ public class Main {
         // 关闭池
         pool.shutdown();
         // 使用isCompletedNormally()方法检查任务是否出错
-        if (task.isCompletedNormally()){
+        if (task.isCompletedNormally()) {
             System.out.println("Main: The process has completed normally.");
         }
 
         // 在增长之后，所有产品的价格应该是12。将价格不是12的所有产品的名称和价格写入到控制台
-        for (int i = 0; i < products.size(); i++){
+        for (int i = 0; i < products.size(); i++) {
             Product product = products.get(i);
             if (product.getPrice() != 12) {
                 System.out.println("Product " + product.getName() + " " + product.getPrice());
@@ -84,13 +84,14 @@ class ProductListGenerator {
 
     /**
      * 接收一个容量参数，返回一个产品列表
+     *
      * @param size
      * @return
      */
     public List<Product> generate(int size) {
         List<Product> ret = new ArrayList<>();
-        for (int i = 0; i < size; i++){
-            Product product=new Product();
+        for (int i = 0; i < size; i++) {
+            Product product = new Product();
             product.setName("Product" + i);
             product.setPrice(10);
             ret.add(product);
@@ -128,7 +129,7 @@ class Task extends RecursiveAction {
         }
         // 递归：如果last和first的差大于或等于10，则创建两个新的Task对象，一个处理产品的前半部分，另一个处理产品的后半部分，然后在ForkJoinPool中，使用invokeAll()方法执行它们
         else {
-            int middle=(last+first)/2;
+            int middle = (last + first) / 2;
             System.out.println("Task: Pending tasks:" + getQueuedTaskCount());
             Task t1 = new Task(products, first, middle, increment);
             Task t2 = new Task(products, middle + 1, last, increment);
@@ -138,8 +139,8 @@ class Task extends RecursiveAction {
     }
 
     private void updatePrices() {
-        for (int i = first; i < last; i++){
-            Product product=products.get(i);
+        for (int i = first; i < last; i++) {
+            Product product = products.get(i);
             product.setPrice(product.getPrice() * (1 + increment));
         }
     }
